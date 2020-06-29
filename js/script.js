c2ceda556077beadaddfeb9cea2069eb90ee667e
vlados <example@gmail.com>
2020-06-29 20:31:19 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$('[data-modal=photo1]').on('click', function() {
+    $('.overlay, #photo1').fadeIn('slow');
+});
+$('.overlay').on('click', function() {
+    $('.overlay, #photo1').fadeOut('slow');
+});
+$('[data-modal=photo2]').on('click', function() {
+    $('.overlay, #photo2').fadeIn('slow');
+});
+$('.overlay').on('click', function() {
+    $('.overlay, #photo2').fadeOut('slow');
+});
+$('[data-modal=photo3]').on('click', function() {
+    $('.overlay, #photo3').fadeIn('slow');
+});
+$('.overlay').on('click', function() {
+    $('.overlay, #photo3').fadeOut('slow');
+});
+$('[data-modal=photo4]').on('click', function() {
+    $('.overlay, #photo4').fadeIn('slow');
+});
+$('.overlay').on('click', function() {
+    $('.overlay, #photo4').fadeOut('slow');
+});
+$('[data-modal=photo5]').on('click', function() {
+    $('.overlay, #photo5').fadeIn('slow');
+});
+$('.overlay').on('click', function() {
+    $('.overlay, #photo5').fadeOut('slow');
+});
+$('[data-modal=photo6]').on('click', function() {
+    $('.overlay, #photo6').fadeIn('slow');
+});
+$('.overlay').on('click', function() {
+    $('.overlay, #photo6').fadeOut('slow');
+});
+
+
